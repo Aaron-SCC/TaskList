36f394b7d3c65df9d7ba4218c92c918634b796d6
Aaron-SCC <aaron.lewis@seattlecentral.edu>
2019-05-07 19:49:07 -0700

    added tasks per Assignment.

--- app/src/main/java/com/example/tasklist/TaskListActivity.java
+++ app/src/main/java/com/example/tasklist/TaskListActivity.java
@@ -35,6 +35,37 @@ public class TaskListActivity extends AppCompatActivity {
             sb.append("Row inserted! Insert Id: " + insertId2 + "\n");
         }
 
+        //// FOR ASSIGNMENT
+        // insert a third task
+        Task task3 = new Task(1, "Take out trash. ID=5", "", "0", "0");
+        long insertId3 = db.insertTask(task3);
+        if (insertId3 > 0) {
+            sb.append("Row inserted! Insert Id: " + insertId3 + "\n");
+        }
+
+        // insert a fourth task
+        Task task4 = new Task(1, "Go to sleep at 10PM. ID=6", "", "0", "0");
+        long insertId4 = db.insertTask(task4);
+        if (insertId4 > 0) {
+            sb.append("Row inserted! Insert Id: " + insertId4 + "\n");
+        }
+
+        // insert a fifth task
+        Task task5 = new Task(1, "Take out trash. ID=7", "", "0", "0");
+        long insertId5 = db.insertTask(task5);
+        if (insertId5 > 0) {
+            sb.append("Row inserted! Insert Id: " + insertId5 + "\n");
+        }
+
+        // insert a sixth task
+        Task task6 = new Task(1, "Take out trash. ID=8", "", "0", "0");
+        long insertId6 = db.insertTask(task6);
+        if (insertId6 > 0) {
+            sb.append("Row inserted! Insert Id: " + insertId6 + "\n");
+        }
+
+
+
         // update a task
         task.setId((int) insertId);
         task.setName("Update test");
